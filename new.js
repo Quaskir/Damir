d35855deb0e4b2a707940a5c482d0731f44bd569
Dias Sunnatilla <diassunnatilla@MacBook-Air-Dias.local>
2024-11-14 20:10:44 +0500

    burger btn

--- new.js
+++ new.js
@@ -24,3 +24,12 @@ document.querySelectorAll('.accordion-button').forEach(button => {
       content.style.display = content.style.display === 'block' ? 'none' : 'block';
     });
   });
+
+
+  const burgerBtn = document.querySelector('.btn-burger')
+  const burgerContent = document.querySelector('.btn-burger-content')
+  console.log(burgerContent)
+  burgerBtn.addEventListener('click', function(){
+      this.classList.toggle('active')
+      burgerContent.classList.toggle('active')
+  })
